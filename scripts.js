60bd6ccd3a1c3bcef67b135e5a4cd7c60f2cece3
dvlpr.eth <12403641+dvlpr-eth@users.noreply.github.com>
2023-01-31 18:02:03 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,99 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Loaded!");
-
-  const db = firebase.firestore();
+  console.log("Ready!");
   const form = document.getElementById("form");
-  const nickname = document.getElementById("nickname");
-  const msg = document.getElementById("message");
-  const chat = document.getElementById("chat");
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const list = document.getElementById("list");
+  const db = firebase.firestore();
+
+  function addUser(first, last) {
+    db.collection("Users")
+      .add({
+        firstName: first,
+        lastName: last,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((docRef) => {
+        console.log("Document added successfully!", docRef.id);
+        getUsers();
+      })
+      .catch((error) => console.log("Error adding document", error));
+  }
+
+  function getUsers() {
+    db.collection("Users")
+      .orderBy("timestamp")
+      .get()
+      .then((querySnapshot) => {
+        let outout = "";
+
+        querySnapshot.forEach((doc) => {
+          console.log("doc", doc.id);
+          outout += `<li>${doc.data().firstName} - ${doc.data().lastName}</li>`;
+        });
+        list.innerHTML = outout;
+      });
+  }
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-
-    if (nickname.value && msg.value) {
-      console.log("form submit!");
-      addChat(nickname.value, msg.value);
-
-      msg.value = "";
+    if (firstName.value && lastName.value) {
+      addUser(firstName.value, lastName.value);
+    } else {
+      alert("Please fill the form!");
     }
   });
 
-  function deleteChat(id) {
-    db.collection("Chat")
-      .doc(id)
-      .delete()
-      .then(function () {
-        console.log("Document successfully deleted!");
-      })
-      .catch(function () {
-        console.log("Error deleting document");
-      });
-  }
-
-  function addChat(nickname, msg) {
-    db.collection("Chat")
-      .add({
-        nickname,
-        msg,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then(function (docRef) {
-        console.log("Document written with ID:", docRef.id);
-      })
-      .catch(function (error) {
-        console.log("Error adding document", error);
-      });
-  }
-
-  function updateChat(id, msg) {
-    db.collection("Chat")
-      .doc(id)
-      .update({
-        msg,
-      })
-      .then(function () {
-        console.log("Document successfully updated!");
-      })
-      .catch(function (error) {
-        console.log("Error updating document", error.message);
-      });
-  }
-
-  function init() {
-    db.collection("Chat")
-      .orderBy("timestamp", "asc")
-      .onSnapshot(function (querySnapshot) {
-        chat.innerHTML = "";
-
-        querySnapshot.forEach((doc) => {
-          const li = document.createElement("li");
-          li.innerHTML = `${doc.data().nickname}: ${doc.data().msg}`;
-          li.style = `
-        background-color: #efefef;
-        font-size: 17px;
-        padding:10px 30px;
-        margin: 10px 0;
-        line-height: 50px;
-        width:300px;
-        border-radius: 40px;
-        position:relative
-        `;
-
-          let span = document.createElement("span");
-          span.innerHTML = "&#10005;";
-          span.style = ` 
-            position: absolute;
-            right: 25px;
-            cursor:pointer
-        `;
-
-          span.addEventListener("click", () => deleteChat(doc.id));
-          li.appendChild(span);
-          chat.appendChild(li);
-        });
-      });
-  }
-  init();
-  //   deleteChat("Zu7UgpOcHMsF5rwHuZok");
-  //   updateChat("Zu7UgpOcHMsF5rwHuZok", "Bye!");
+  getUsers();
 });
